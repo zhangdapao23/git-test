0dd0ba19defe00e171650c9b8b0528655d110446
zc <1059970698@qq.com>
2022-03-18 22:57:47 +0800

    Initial commit

---
+++ src/main/java/com/zc/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zc.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
+        System.out.println("hello git3!!!");
+        System.out.println("hello git4!!!");
+        System.out.println("master test!!!");
+        System.out.println("hot-fix test!");
+    }
+}
